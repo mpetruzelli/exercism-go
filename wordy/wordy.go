4fddd92a5b3505d32130efc73a1aad7d9c45f48b
psimoes <p.simoes@campus.fct.unl.pt>
2017-12-12 14:14:53 +0000

    wordy solved by using odd numbers

--- wordy/wordy.go
+++ wordy/wordy.go
@@ -1,52 +1,9 @@
 package wordy
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
-
-type Tree struct {
-	Left  *Tree
-	Value string
-	Right *Tree
-}
-
-// Walk traverses a tree depth-first,
-// sending each Value on a channel.
-func Walk(t *Tree, ch chan string) {
-	if t == nil {
-		return
-	}
-	Walk(t.Left, ch)
-	ch <- t.Value
-	Walk(t.Right, ch)
-}
-
-// Walker launches Walk in a new goroutine,
-// and returns a read-only channel of values.
-func Walker(t *Tree) <-chan string {
-	ch := make(chan string)
-	go func() {
-		Walk(t, ch)
-		close(ch)
-	}()
-	return ch
-}
-func insert(t *Tree, lr, v string) *Tree {
-	if t == nil {
-		return &Tree{nil, v, nil}
-	}
-	switch lr {
-	case "left":
-		t.Left = insert(t.Left, "right", v)
-		return t
-	case "right":
-		t.Right = insert(t.Right, "left", v)
-		return t
-	}
-	return t
-}
 
 var operation = map[string]bool{
 	"plus":       true,
@@ -54,21 +11,6 @@ var operation = map[string]bool{
 	"multiplied": true,
 	"divided":    true,
 	"raised":     true,
-}
-
-//creates new binary tree inserting left,right,left,right
-func New(split []string) *Tree {
-	var t *Tree
-	var order string = "right"
-	for _, v := range split {
-		t = insert(t, order, v)
-		if order == "left" {
-			order = "right"
-		} else {
-			order = "left"
-		}
-	}
-	return t
 }
 
 func Answer(s string) (int, bool) {
@@ -89,19 +31,40 @@ func Answer(s string) (int, bool) {
 	if len(numbersandoperations) < 3 {
 		return result, false
 	}
+	var op string
+	for k, v := range numbersandoperations {
 
-	tree := New(numbersandoperations)
+		if k%2 == 0 {
+			n, err := strconv.Atoi(v)
+			if err != nil {
 
-	ch := Walker(tree)
-	for {
-		x, ok := <-ch
-		fmt.Println("ch1", x, ok)
-		if !ok {
-			ch = nil
+			} else {
+
+				switch op {
+				case "plus":
+					result += n
+					break
+				case "minus":
+					result -= n
+					break
+				case "multiplied":
+					result *= n
+					break
+				case "divided":
+					result /= n
+					break
+				case "raised":
+					break
+				default:
+					result += n
+
+				}
+			}
+		} else {
+			op = v
 		}
-		if ch == nil {
-			break
-		}
+
 	}
+
 	return result, true
 }
