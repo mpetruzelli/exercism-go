d036962c93a099c54c2077e89c3de89089f6827a
psimoes <p.simoes@campus.fct.unl.pt>
2017-12-16 16:36:01 +0000

    binary search tree completted

--- binary-search-tree/binary_search_tree.go
+++ binary-search-tree/binary_search_tree.go
@@ -27,16 +27,55 @@ func (t *SearchTreeData) Insert(v int) {
 
 }
 
+// Walk traverses a tree depth-first,
+// sending each Value on a channel.
+func (t *SearchTreeData) Walk(ch chan int) {
+	if t == nil {
+		return
+	}
+	t.left.Walk(ch)
+	ch <- t.data
+	t.right.Walk(ch)
+}
+
+// Walker launches Walk in a new goroutine,
+// and returns a read-only channel of values.
+func (t *SearchTreeData) Walker() <-chan int {
+	ch := make(chan int)
+	go func() {
+		t.Walk(ch)
+		close(ch)
+	}()
+	return ch
+}
+
 func Bst(v int) SearchTreeData {
 	var t SearchTreeData
 	t = SearchTreeData{nil, v, nil}
 	return t
 }
 
-func (*SearchTreeData) MapString(func(int) string) []string {
-	return []string{}
+func (t *SearchTreeData) MapString(x func(int) string) (out []string) {
+	c1 := t.Walker()
+	for {
+		v1, ok := <-c1
+		if !ok {
+			break
+		}
+		out = append(out, x(v1))
+	}
+	return
 }
 
-func (*SearchTreeData) MapInt(func(int) int) []int {
-	return []int{}
+func (t *SearchTreeData) MapInt(x func(int) int) (out []int) {
+	c1 := t.Walker()
+	for {
+		v1, ok := <-c1
+		if !ok {
+			break
+		}
+		out = append(out, x(v1))
+	}
+
+	return
 }
